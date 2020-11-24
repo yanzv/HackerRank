ff24d29194a2d0194e45ab25296a761d3cee36e8
Sumit Gupta <41279402+sumitgupta1312@users.noreply.github.com>
2020-11-24 15:24:48 +0530

    Update BigInteger.java

--- Java/Big Number/BigInteger.java
+++ Java/Big Number/BigInteger.java
@@ -18,7 +18,7 @@ import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 
-public class BigInteger {
+public class Solution {
 
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
