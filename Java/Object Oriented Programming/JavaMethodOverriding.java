cb40ad650a62e933b71c6c56599f396c575af730
Sumit Gupta <41279402+sumitgupta1312@users.noreply.github.com>
2020-11-24 14:31:24 +0530

    Update JavaMethodOverriding.java

--- Java/Object Oriented Programming/JavaMethodOverriding.java
+++ Java/Object Oriented Programming/JavaMethodOverriding.java
@@ -40,37 +40,37 @@ Each team has 11 players in Soccer Class
 import java.util.*;
 class Sports{
 
-   String get_name()
-   {
-      return "Generic Sports";
-   }
-   void get_number_of_team_members()
-   {
-      System.out.println("Each team has n players in "+get_name());
-   }
+    String getName(){
+        return "Generic Sports";
+    }
+  
+    void getNumberOfTeamMembers(){
+        System.out.println( "Each team has n players in " + getName() );
+    }
 }
 
-class Soccer extends Sports
-{
-   String get_name()
+class Soccer extends Sports{
+    @Override
+    String getName(){
+        return "Soccer Class";
+    }
+
+    // Write your overridden getNumberOfTeamMembers method here
+void getNumberOfTeamMembers()
    {
-      return "Soccer Class";
+      System.out.println("Each team has 11 players in "+getName());
    }
-    void get_number_of_team_members()
-   {
-      System.out.println("Each team has 11 players in "+get_name());
-   }
+
 }
-public class JavaMethodOverriding
-{
-   
-   public static void main(String []args)
-   {
-      Sports C1=new Sports();
-      Soccer C2=new Soccer();
-      System.out.println(C1.get_name());
-      C1.get_number_of_team_members();
-      System.out.println(C2.get_name());
-      C2.get_number_of_team_members();
-   }
+
+public class Solution{
+	
+    public static void main(String []args){
+        Sports c1 = new Sports();
+        Soccer c2 = new Soccer();
+        System.out.println(c1.getName());
+        c1.getNumberOfTeamMembers();
+        System.out.println(c2.getName());
+        c2.getNumberOfTeamMembers();
+	}
 }
